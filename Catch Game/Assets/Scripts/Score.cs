f896832062d9aa653ebae78ed9e860866e29bba9
LAUREN CARR <CARR.LAUREN.CA@GMAIL.COM>
2017-09-15 13:31:39 -0700

    CS485 Assignment 1

---
+++ Catch Game/Assets/Scripts/Score.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+
+
+	public Text scoreText;
+	public int appleValue;
+
+	private int score;
+
+	// Use this for initialization
+	void Start () {
+		score = 0;
+		UpdateScore ();
+	}
+
+	void OnTriggerEnter2D () {
+		score += appleValue;
+		UpdateScore ();
+	}
+		
+	void UpdateScore () {
+		scoreText.text = "Score:\n" + score;
+	}
+}
